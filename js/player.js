b4d7cca61666f6da06aae5020fd3c226d34643bd
again <935669873@qq.com>
2021-02-26 18:35:25 +0800

    Update CNAME

---
+++ js/player.js
@@ -0,0 +1,15 @@
+
+// mePlayer({
+//   music: {
+//     src: '/music/红豆.mp3',
+//     title: '晚风',
+//     author: '纯音乐请欣赏',
+//     loop: true
+//   },
+//   target: '.music',
+//   autoplay: false
+// })
+  
+// document.querySelector('button').addEventListener('click', function() {
+//   mePlayer.toggleTheme()
+// })
